7baae0885e1cef58ec86624f2f1ebfeb341cc9c3
mohammad <usermp76@gmail.com>
2025-08-01 02:27:38 +0330

    [feat] Update SendSmsToken job with KavenegarService and logging.

--- src/Jobs/SendSmsToken.php
+++ src/Jobs/SendSmsToken.php
@@ -2,9 +2,9 @@
 
 namespace LotousOrganization\LaravelLogin\Jobs;
 
-use Sentry\SentrySdk;
-use Kavenegar\KavenegarApi;
+use App\Services\KavenegarService;
 use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -14,30 +14,27 @@ class SendSmsToken implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $phoneNumber;
-    protected $otp;
+    protected string $phoneNumber;
+    protected string $otp;
 
-    /**
-     * Create a new job instance.
-     */
-    public function __construct(string $phoneNumber, $otp)
+    public function __construct(string $phoneNumber, string $otp)
     {
         $this->phoneNumber = $phoneNumber;
         $this->otp = $otp;
     }
 
-    /**
-     * Execute the job.
-     */
-    public function handle(): void
+    public function handle(KavenegarService $kavenegarService): void
     {
-        $apiKey = env("KAVENEGAR_API_KEY");
-        $api = new KavenegarApi($apiKey);
-        try {
-            $api->Send(env('KAVEHNEGAR_SENDER'), $this->phoneNumber, $this->otp);
-        } catch (\Exception $e) {
-            SentrySdk::init();
-            SentrySdk::getCurrentHub()->captureException($e);
+        $template = env("KAVENEGAR_TEMPLATE");
+
+        $response = $kavenegarService->sendOtp(
+            $this->phoneNumber,
+            $this->otp,
+            $template
+        );
+
+        if (!$response) {
+            Log::warning("Failed to send OTP to {$this->phoneNumber}");
         }
     }
 }
