015cbf3bf7a036f0e2b8a724f05a54d15dedbe4a
mohammad <usermp76@gmail.com>
2025-08-01 02:23:10 +0330

    [feat] Implement KavenegarService for sending OTP via SMS.

---
+++ src/app/Services/KavenegarService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace LotousOrganization\LaravelLogin\app\Services;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Sentry\SentrySdk;
+
+class KavenegarService
+{
+    protected string $apiKey;
+
+    public function __construct()
+    {
+        $this->apiKey = env("KAVENEGAR_API_KEY");
+    }
+
+    public function sendOtp(string $phoneNumber, string $otp, string $template): ?array
+    {
+        $url = "https://api.kavenegar.com/v1/{$this->apiKey}/verify/lookup.json";
+
+        $response = Http::timeout(20)->get($url, [
+            'receptor' => $phoneNumber,
+            'token'    => $otp,
+            'template' => $template,
+        ]);
+
+        if ($response->successful()) {
+            Log::info('Kavenegar OTP sent.', $response->json());
+            return $response->json();
+        }
+
+        $this->reportError($response);
+        return null;
+    }
+
+    protected function reportError($response): void
+    {
+        $error = $response->json();
+        Log::error('Kavenegar API Error', [
+            'status' => $response->status(),
+            'response' => $error,
+        ]);
+
+        SentrySdk::init();
+        SentrySdk::getCurrentHub()->captureMessage("Kavenegar API Error: " . json_encode($error));
+    }
+}
